aca4811f900e02eb56acebf794b4d0e9c0be95c8
Youssef Oufaska <youssef.oufaska@gmail.com>
2020-08-17 00:11:37 +0200

    test

--- my-app-vue-potions/src/App.vue
+++ my-app-vue-potions/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Inicio</router-link> |
+      <router-link to="/about">Sobre</router-link>
     </div>
     <router-view/>
   </div>
