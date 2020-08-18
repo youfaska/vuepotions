7e80b1fdd2fa410a6e0bb2d81a67cdc878f9246e
youfaska <youssef.oufaska@gmail.com>
2020-08-19 00:08:14 +0200

    Update App.vue

--- my-app-vue-potions/src/App.vue
+++ my-app-vue-potions/src/App.vue
@@ -24,7 +24,7 @@
       <v-spacer></v-spacer>
 
       <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Cerrar Session</span>
+       
         <v-icon>fas fa-sign-out-alt</v-icon>
       </v-btn>
     </v-app-bar>
