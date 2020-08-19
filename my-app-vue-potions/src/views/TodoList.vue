fbf0e9d5a6a8bba5748db1ce8143f60f3d009c01
Youssef Oufaska <youssef.oufaska@gmail.com>
2020-08-20 01:16:49 +0200

    Gestion de tareas con una tabla

--- my-app-vue-potions/src/views/TodoList.vue
+++ my-app-vue-potions/src/views/TodoList.vue
@@ -11,10 +11,33 @@
             outlined
             required
           ></v-text-field>
-          {{newTask}}
         </v-col>
         <v-col cols="12" md="6">
           <v-btn type="submit" color="primary" :disabled="!valid">Crear tarea</v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col col="12">
+          <h2 class="text-center">TAREAS</h2>
+          <v-simple-table :dense="false" :fixed-header="false" height="300">
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">Name</th>
+                  <th class="text-left">Calories</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(item, index) in tasks" :key="index">
+                  <td>
+                    <v-icon color="success" @click="completarTarea(item.text)" v-if="item.completed">far fa-check-square</v-icon>
+                    <v-icon @click="completarTarea(item.text)" v-else>far fa-square</v-icon>
+                  </td>
+                  <td>{{ item.text }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
         </v-col>
       </v-row>
     </v-container>
@@ -44,6 +67,15 @@ export default {
       this.tasks.push(task);
       this.newTask = "";
       console.log(this.tasks);
+    },
+    completarTarea(taskText) {
+      console.log("entrando en la funcion de completar tareas");
+      for (let i = 0; i < this.tasks.length; i++) {
+        let task = this.tasks[i];
+        if (taskText === task.text) {
+          task.completed = !task.completed;
+        }
+      }
     }
   }
 };
